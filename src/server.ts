6aaa26f9b8f25fb92ab5f3dceecff18a6fb907cd
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2023-01-10 00:39:46 -0300

    The base

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+
+
+dotenv.config();
+const server = express();
+
+server.use(cors());
+server.use(express.static(path.join(__dirname, "../public")));
+server.use(express.urlencoded({extended:true}));
+
+
+server.use((req, res)=>{
+    res.status(404).json({error: 'Page not found'});
+});
+
+server.listen(process.env.PORT);
